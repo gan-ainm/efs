99a10161f4bda092b7891ab966092096f4bd5c0b
Gan Ainm <gan.ainm.riomhphost@gmail.com>
2021-10-30 21:05:43 +0200

    Add init.el as default argument

--- runemacs.sh
+++ runemacs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/emacs -Q --eval '(setq package-user-dir "~/Projects/efs/packages")' --eval '(setq user-emacs-directory "~/Projects/efs")' -l ~/Projects/efs/init.el
+/usr/bin/emacs -Q --eval '(setq package-user-dir "~/Projects/efs/packages")' --eval '(setq user-emacs-directory "~/Projects/efs")' -l ~/Projects/efs/init.el init.el
