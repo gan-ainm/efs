e411ede2e7833454790083280ae882ec0e25d874
Gan Ainm <gan.ainm.riomhphost@gmail.com>
2021-10-31 14:33:54 +0100

    Re-insert use-package forge

--- init.el
+++ init.el
@@ -190,6 +190,8 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package forge)
+
 (defun efs/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
